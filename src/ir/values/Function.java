2ecbfa041c64f02815742c2d594fb50e01f5ebed
hjc-owo <2535199657@qq.com>
2022-12-09 21:34:56 +0800

    Argument放到Function的内部

--- src/ir/values/Function.java
+++ src/ir/values/Function.java
@@ -222,4 +222,27 @@ public class Function extends Value {
         s.append(")");
         return s.toString();
     }
+
+    public static class Argument extends Value {
+        private int index;
+
+        public Argument(String name, Type type, int index) {
+            super(name, type);
+            this.index = index;
+        }
+
+        public Argument(Type type, int index, boolean isLibraryFunction) {
+            super(isLibraryFunction ? "" : "%" + REG_NUMBER++, type);
+            this.index = index;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        @Override
+        public String toString() {
+            return this.getType().toString() + " " + this.getName();
+        }
+    }
 }
